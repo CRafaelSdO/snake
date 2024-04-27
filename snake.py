4b8e709a33be389bc9c576dc7b4f558dfc9695f1
C. Rafael Oliveira <crafaelsdo@gmail.com>
2024-04-27 19:56:59 -0300

    Initial commit

---
+++ snake.py
@@ -0,0 +1,16 @@
+""" Módulo de lançamento do jogo """
+
+# Importação externa
+from arcade import run
+
+def main():
+    """ Função Principal """
+
+    # Cria e configura a janela
+
+    # inicia o loop principal
+    run()
+
+# Inicia a função principal caso este modulo esteja sendo chamado
+if __name__ == "__main__":
+    main()
